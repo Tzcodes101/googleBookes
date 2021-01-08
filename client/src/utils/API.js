eb33efa99c3992c3f6fa4de9d44409fa6c12ac9b
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-08 15:49:20 -0500

    utils API.js code added

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,11 +2,22 @@ import axios from "axios";
 
 export default {
     //books from the google API
+    getBooks: function(q) {
+        return axios.get("/api/google", { params: { q: "title:" + q} });
+    },
 
     //get saved books (our own "api")
+    getSavedBooks: function() {
+        return axios.get("/api/books");
+    },
 
-    
     //delete saved book (based off id)
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
+    },
 
-    //svae book to database (the "api" we create)
+    //save book to database (the "api" we create)
+    saveBook: function(bookInfo) {
+        return axios.post("/api/books", bookInfo);
+    }
 }
