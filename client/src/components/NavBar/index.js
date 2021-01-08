f097643ebf9a278015d7426c6b1d6d97ed9248ce
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-08 12:51:11 -0500

    beginning of nav comp. code

---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>I am going to be a fancy nav...one day!</div>
+        )
+    }
+}
+
+export default NavBar;
