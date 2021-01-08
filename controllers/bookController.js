dd675049bcb430f814d6de232c199df7e39f49aa
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-08 15:55:43 -0500

    bookController code added

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,12 +3,28 @@ const db = require("../models");
 //define methods
 module.exports = {
     //findAll
+    findAll: function(req, res) {
+        db.Book.find(req.query)
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    },
 
     //findbyId
+    findById: function (req, res) {
+        db.Book.find(req.params.id)
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    },
 
-    //create
+    //create (to do)
 
-    //update
+    //update (to do)
 
-    //remove
+    //delete a book based off id
+    delete: function(req, res) {
+        db.Book.findById(req.params.id)
+            .then(dbBook => dbBook.remove())
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    }
 }
