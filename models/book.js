8204976e5e532b863ab2a7c7b656d4bc5c57ff1e
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-08 11:57:15 -0500

    book model added

--- models/book.js
+++ models/book.js
@@ -1,7 +1,18 @@
 //dependencies
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-//book schema with title, subtitle, authors, link, despcription, image, googleID
+//book schema with title, authors, link, despcription, image
+const bookSchema = new Schema({
+    title: { type: String, required: true },
+    authors: { type: [String], required: true },
+    link: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+});
 
 //set schema as a const
+const Book = mongoose.model("Book", boookSchema);
 
 //export book
+module.exports = Book; 
