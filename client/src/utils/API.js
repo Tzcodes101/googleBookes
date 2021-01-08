2c5278160868fa81b84a2482a54f10937c45658b
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-08 15:43:25 -0500

    utils API pseudocode

---
+++ client/src/utils/API.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default {
+    //books from the google API
+
+    //get saved books (our own "api")
+
+    
+    //delete saved book (based off id)
+
+    //svae book to database (the "api" we create)
+}
