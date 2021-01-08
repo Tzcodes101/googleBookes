1f295d29dd5279e242e8b2699959c465a3f901ae
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-08 12:43:46 -0500

    beginning of components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path={["/", "/search"]} component={Search} />
+          <Route exact path="/saved" component={Saved} />
+          <route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
