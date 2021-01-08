f9ac91aeaea98e8131fa116f01cc6c92cbe8346e
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-08 16:19:49 -0500

    Search.js pseudocode

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,11 +3,38 @@ import Jumbotron from "../components/Jumbotron";
 import Footer from "../components/Footer";
 
 class Search extends Component {
+    //create state for books, q, and message
+
+
+    //component mounts
+
+    //get books (from google)
+        //set book state to response
+
+    //handleInputChange (when user searches for a book from the googleAPI)
+        //change state with name to value
+
+    //handleFormSubmit
+        //call getBooks
+
+    //saveBook
+        //find book by id
+        //use API.saveBook, putting in all needed info from the data object that we need for the Book model
+
+    
 
 
     render() {
         return (
+            //create grid
+            <div>
+            <Jumbotron>
+                <h1>React Google Book Search</h1>
+            </Jumbotron>
+            {/* Form to Search */}
+            {/* Card to display results */}
             <div>Search</div>
+            </div>
         )
     }
 }
