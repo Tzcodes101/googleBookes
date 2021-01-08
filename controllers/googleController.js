1cad64620e843191b1bf18983f35143a98b8f5a4
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-08 16:06:06 -0500

    googleController code addition

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -5,5 +5,20 @@ const db = require("../models");
 
 
 module.exports = {
-    //findAll searches entire API but returns ones not already saved
+    //findAll searches entire API but returns ones not already saved (at some point in time)
+    findAll: function (req, res) {
+        const { query: params } = req;
+        axios.get("https://www.googleapis.com/books/v1/volumes", { params })
+            .then(results => results.data.items.filter(
+                result =>
+                    result.volumeInfo.title &&
+                    result.volumeInfo.authos &&
+                    result.volumeInfo.description &&
+                    result.volumeInfo.infoLink
+            ))
+            .then(googleBooks => {
+                    res.json(googleBooks)
+                    .catch(err => res.status(422).json(err));
+            })
+        }
 };
