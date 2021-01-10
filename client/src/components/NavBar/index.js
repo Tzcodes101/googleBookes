cf02de5fba82105644fda82e86dc65057d9b0dca
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-09 21:40:15 -0500

    dynamic navbar code added

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,7 +1,41 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 class NavBar extends Component {
+
+    state = {
+        open: false,
+        width: window.innerWidth
+    }
+
+    //change window width state depending on windoe size 
+    updateWidth = () => {
+        const newState = { width: window.innerWidth };
+
+        if (this.state.open && newState.width > 991) {
+            newState.open = false;
+        }
+
+        this.setState(newState);
+    };
+
+    //set state of open to the opposite of it's current state
+    toggleNav = () => {
+        this.setState({ open: !this.state.open });
+    };
+
+    //when component mounts, resize depending on window width
+    componentDidMount() {
+        window.addEventListener("resize", this.updateWidth);
+    };
+
+    //when component will unmount, resize depending on window width
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateWidth);
+    }
+
+
     render() {
         return (
             <div>I am going to be a fancy nav...one day!</div>
