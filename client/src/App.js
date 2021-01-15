738e6ac7ee340e48cea722a77453c03a9dda783f
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-01-15 14:09:53 -0500

    added index to search.js import for debugging

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search";
-import Saved from "./pages/Saved";
+import Saved from "./pages/Saved/index";
 import NoMatch from "./pages/NoMatch";
 import NavBar from "./components/NavBar";
 
